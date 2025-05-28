ecdb398ce1cfa2d51436f6f1ef3d8a061d82a633
Caio César <caiogalaxy00@gmail.com>
2025-05-28 13:49:23 -0300

    feat: criar fluxo inicial de atualização de cliente

---
+++ src/main/java/com/caio/hexagonal/application/core/usecase/UpdateCustomerUseCase.java
@@ -0,0 +1,33 @@
+package com.caio.hexagonal.application.core.usecase;
+
+import com.caio.hexagonal.application.core.domain.Customer;
+import com.caio.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
+import com.caio.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+import com.caio.hexagonal.application.ports.out.UpdateCustomerOutputPort;
+
+public class UpdateCustomerUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+
+    private final UpdateCustomerOutputPort updateCustomerOutputPort;
+
+    public UpdateCustomerUseCase(
+            FindCustomerByIdOutputPort findCustomerByIdOutputPort,
+            FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
+            UpdateCustomerOutputPort updateCustomerOutputPort
+    ) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.updateCustomerOutputPort = updateCustomerOutputPort;
+    }
+
+    public void update(Customer customer, String zipCode) {
+        findCustomerByIdOutputPort.find(customer.getId());
+        var address = findAddressByZipCodeOutputPort.find(zipCode);
+        customer.setAddress(address);
+        updateCustomerOutputPort.update(customer);
+    }
+
+}
