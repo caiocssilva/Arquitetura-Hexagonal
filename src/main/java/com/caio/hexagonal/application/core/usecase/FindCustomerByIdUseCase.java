48d6d63199e0cf254410b5c5e3de0158f3040e1c
Caio César <caiogalaxy00@gmail.com>
2025-05-28 13:06:45 -0300

    feat: criar use case e output port para buscar cliente por ID

---
+++ src/main/java/com/caio/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -0,0 +1,19 @@
+package com.caio.hexagonal.application.core.usecase;
+
+import com.caio.hexagonal.application.core.domain.Customer;
+import com.caio.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
+public class FindCustomerByIdUseCase {
+
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
+    public Customer find(String id) {
+        return findCustomerByIdOutputPort.find(id)
+                .orElseThrow(() -> new  RuntimeException("Customer not found"));
+    }
+
+}
